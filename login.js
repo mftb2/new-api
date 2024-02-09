df90279f8b7deb7adb7e720dd2751fd30fffdd1c
Dallen Harmon <dallenzao@yahoo.com>
2024-02-08 19:42:14 -0700

    added new login

---
+++ login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+export default function Login(props){
+    return(
+        <View style={styles.login}>
+            <Text>Button moment</Text>
+            <Button title="Hi Dallen" onPress={()=>{}}></Button>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'green',
+        height: '12%',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        
+      },
+})
